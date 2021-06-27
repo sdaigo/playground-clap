6a005a90db6e1163e7fa180dbb79c39a5fdd3376
Daigo Shitara <stain.witness+github@gmail.com>
2021-06-27 12:05:32 +0900

    handle subcomands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{load_yaml, App};
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from(yaml).get_matches();
+
+    match matches.subcommand_name() {
+        Some("media") => {
+            // detect subcommand Lv1:
+            // ex) % app media
+            let media = matches.subcommand_matches("media").unwrap();
+
+            // detect subcommand Lv2:
+            // ex) % app media convert
+            let convert = media.subcommand_matches("convert").unwrap();
+
+            // it's safe to unwrap since `file` argument is required by clap
+            println!("file: {}", convert.value_of("file").unwrap());
+        }
+
+        Some("fortune") => {
+            let _fortune = matches.subcommand_matches("fortune").unwrap();
+
+            println!("fortune is run");
+        }
+
+        None => println!("No subcommand was used"),
+
+        _ => println!("??"),
+    }
 }
